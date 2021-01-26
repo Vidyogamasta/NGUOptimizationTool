fc7df9b403d4a52ab661958fb6e3db18f0c8a611
Andrew Shirley <vidyogamaster@yahoo.com>
2021-01-25 22:10:45 -0600

    Add project files.

---
+++ NGUOptimizationTool/FruitData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NGUOptimizationTool
+{
+    public class FruitData
+    {
+        public FruitData(string name, int baseCost)
+        {
+            Name = name;
+            BaseCost = baseCost;
+        }
+
+        public string Name { get; set; }
+        public long BaseCost { get; set; }
+        public long SeedsSpent(long level)
+        {
+            return BaseCost * level * (level + 1) * (2 * level + 1) / 6;
+        }
+    }
+}
---
+++ NGUOptimizationTool/StateContainer.cs
@@ -0,0 +1,33 @@
+﻿using Blazored.LocalStorage;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NGUOptimizationTool
+{
+    public class StateContainer
+    {
+
+        public event Action OnChange;
+
+        public StateContainer(ISyncLocalStorageService localStorage)
+        {
+            var data =  localStorage.GetItemAsString("playerData");
+            if(data != null)
+            {
+                var settings = new JsonSerializerSettings()
+                {
+                    ObjectCreationHandling = ObjectCreationHandling.Replace
+                };
+                PlayerData = JsonConvert.DeserializeObject<PlayerData>(data, settings);
+            }
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+        private PlayerData _playerData;
+        public PlayerData PlayerData { get => _playerData; set { _playerData = value; NotifyStateChanged(); } }
+    }
+}
